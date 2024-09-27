97dff9c7e28e2c70dbff4cffc23fb4baa26eef7d
aghadiayeamyanvboernest <ama24004@byui.edu>
2024-09-27 15:14:20 +0100

    update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,6 +2,6 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
 
-// Get the last modified date of the document
+// Get the last modified date of the document   
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = "Last Modification: " + lastModified;
